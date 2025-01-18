fe159054c10632d6c63392f50ec0f684252ab05f
nilesh <nileshmaurya879@gmail.com>
2025-01-18 23:47:21 +0530

    Added home page template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { KnowmoreComponent } from './knowmore/knowmore.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { MenuCardComponent } from './menu-card/menu-card.component';
+import { ChartsDashboardComponent } from './charts-dashboard/charts-dashboard.component';
+import { RestaurantDashboardComponent } from './restaurant-dashboard/restaurant-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+   {
+        path:"",
+        component:RestaurantDashboardComponent
+   },
+   {
+       path:"knowmore",
+       component:KnowmoreComponent
+   },
+   {
+        path:"login",
+        component:LoginComponent
+   },
+   {
+        path:"signup",
+        component:SignupComponent
+   },
+   {
+        path:"inventory",
+        component:InventoryComponent
+   },
+   {
+        path:'menu',
+        component:MenuCardComponent
+   },
+   {
+        path:"chart",
+        component:ChartsDashboardComponent
+   }
+];
