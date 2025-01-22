68a0b1269bfeca7e6dc42fcd905b7a5402c73f7a
nilesh <nileshmaurya879@gmail.com>
2025-01-22 23:09:37 +0530

    Added restaurant dashboard

--- src/app/restaurant-dashboard/restaurant-dashboard.component.ts
+++ src/app/restaurant-dashboard/restaurant-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-restaurant-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './restaurant-dashboard.component.html',
   styleUrl: './restaurant-dashboard.component.css'
 })
