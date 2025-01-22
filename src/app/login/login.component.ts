f8b25af0303c7ae9e738c7561cceb9d6450f57c8
nilesh <nileshmaurya879@gmail.com>
2025-01-23 00:20:41 +0530

    Added login function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginform:FormGroup;
 
+
+  constructor(private fb:FormBuilder){
+    this.loginform = this.fb.group(
+      {
+          email: "",
+          password:""
+      },
+
+    );
+  }
+  Userlogin(){
+
+    console.log(this.loginform.value.email)
+  }
 }
