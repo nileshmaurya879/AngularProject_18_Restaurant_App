d2a9db25eaefb101ba9b25c1e08e1a773292130f
nilesh <nileshmaurya879@gmail.com>
2025-01-27 22:46:15 +0530

    Added restaurant dashboard

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  signupform:FormGroup;
+  constructor(private fb:FormBuilder){
+    this.signupform = this.fb.group (
+      {
+        UserName:"",
+        email:"",
+        password:""
+      }
+    )
+  }
+  UserSignUp(){
 
+  }
 }
