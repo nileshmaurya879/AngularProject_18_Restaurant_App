f6f1d18dd4df2893467aa88840202929f6ef2a4a
nilesh <nileshmaurya879@gmail.com>
2025-03-09 15:21:54 +0530

    Added form group

--- src/app/add-staff-member/add-staff-member.component.ts
+++ src/app/add-staff-member/add-staff-member.component.ts
@@ -1,16 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-staff-member',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule, ReactiveFormsModule],
   templateUrl: './add-staff-member.component.html',
   styleUrl: './add-staff-member.component.css'
 })
+
 export class AddStaffMemberComponent {
   isOpen: boolean = false;
-
+   
+  openModel = new FormGroup({
+      name:new FormControl("")
+  });
   showModal: boolean = false;
 
   openModal() {
@@ -20,5 +25,9 @@ export class AddStaffMemberComponent {
 
   closeModal() {
     this.showModal = false;
+  }
+  addStaffMember(){
+    console.log(this.openModel.value)
+    alert("hello")
   }
 }
